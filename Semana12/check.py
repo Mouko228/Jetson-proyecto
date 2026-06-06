4e2b7a14123fe7e6b5d0819f55fb7451948bd810
Mouko <srj.okabe@gmail.com>
2026-06-06 17:59:14 -0600

    Semana 12, jueves. Agregadas librerias de numpy y OpenCV

---
+++ Semana12/check.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+
+print("Importado Numpy")
+print("Importado OpenCV")
+print(f"-> OpenCV cargado con exito. Version: {cv2.__version__}")
+print(f"-> NumPy acoplado con exito. Version: {np.__version__}")
